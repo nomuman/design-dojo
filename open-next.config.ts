226afca14832957bdbc070656a67cc5d39d7b202
github nomuman <nomuman625@gmail.com>
2026-05-17 23:46:33 +0900

    fix: prevent infinite build loop by setting buildCommand in open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,12 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
+const config = defineCloudflareConfig({
 	// For best results consider enabling R2 caching
 	// See https://opennext.js.org/cloudflare/caching for more details
 	// incrementalCache: r2IncrementalCache
 });
+
+// @ts-ignore buildCommand is not in the type definition but is used at runtime by @opennextjs/aws
+config.buildCommand = "next build";
+
+export default config;
